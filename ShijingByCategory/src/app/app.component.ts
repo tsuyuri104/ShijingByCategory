27c79af3109d8da46d9485c2cc8060ec3a97449c
tsuyuri104 <tsuyuri104@gmail.com>
2020-03-20 23:58:10 +0900

    主な処理の実装完了

---
+++ ShijingByCategory/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Shijing } from './json/shijing.json';
+import { Topic } from './json/topic.json';
+import { promise } from 'protractor';
+import { collectExternalReferences } from '@angular/compiler';
+
+interface IPoemCard {
+  title: string,
+  poems: Array<string>,
+  topic: Array<string>,
+  liuyi: Array<string>
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = '从类别搜索诗经的诗';
+  shijing: Array<IPoemCard> = Shijing;
+  topic = Topic;
+
+  onClick(chekedValues: RadioNodeList) {
+
+    let targetPoems: Array<IPoemCard> = [];
+    let cntChecked: number = 0;
+
+    chekedValues.forEach(element => {
+
+      let chk: HTMLInputElement = <HTMLInputElement>element;
+
+      if (chk.checked) {
+
+        cntChecked += 1;
+
+        Shijing.filter(poem => {
+          return poem.topic.filter(t => {
+            return t === <string>chk.value;
+          }).length > 0;
+        }).forEach(poem => {
+          targetPoems.push(poem);
+        });
+
+      }
+
+    });
+
+    this.shijing = targetPoems;
+  }
+}
