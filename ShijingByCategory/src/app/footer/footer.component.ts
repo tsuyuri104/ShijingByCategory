c324993b8d42aaffa6c858404acc4f4d64bb6f1f
tsuyuri104 <tsuyuri104@gmail.com>
2020-03-29 12:16:38 +0900

    フッター作成

--- ShijingByCategory/src/app/footer/footer.component.ts
+++ ShijingByCategory/src/app/footer/footer.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Reference } from "../json/reference.json";
+import { Update } from "../json/update.json";
+import { version } from "../../../package.json";
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
+
+  ref = Reference;
+  log = Update;
+  ver = version;
 
   constructor() { }
 
