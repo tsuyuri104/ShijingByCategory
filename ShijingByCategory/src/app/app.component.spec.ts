be2e3f0d1666d05f4db78b64ab952b5da74ea77e
tsuyuri104 <tsuyuri104@gmail.com>
2020-03-29 12:16:18 +0900

    app整備

--- ShijingByCategory/src/app/app.component.spec.ts
+++ ShijingByCategory/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ShijingByCategory'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShijingByCategory');
-  });
+  // it(`should have as title 'ShijingByCategory'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('ShijingByCategory');
+  // });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ShijingByCategory app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('ShijingByCategory app is running!');
+  // });
 });
